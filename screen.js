5a2faaee9a509185b720482818897cc21838796a
Pieter <pieter@nr8.nl>
2011-11-17 22:28:32 +0100

    Added screen.js

---
+++ screen.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+var getCumulativeOffset = function (obj) {
+    var left, top;
+    left = top = 0;
+    if (obj.offsetParent) {
+        do {
+            left += obj.offsetLeft;
+            top  += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+    }
+    return {
+        x : left,
+        y : top
+    };
+};
