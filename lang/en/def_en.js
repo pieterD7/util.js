3b4a74db20d5adba7961e1ac8c2d38591d7de87c
Pieter <pieter@nr8.nl>
2011-11-20 02:16:32 +0100

    Added default strings (en)

---
+++ lang/en/def_en.js
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+
+util.defaultStrings = 
+{
+	year: 'year',
+	years: 'years',
+	month: 'month',
+	months: 'months',
+	week: 'week',
+	weeks: 'weeks',
+	day: 'day',
+	days: 'days',
+	hour: 'hour',
+	hours: 'hours',
+	minute: 'minute',
+	minutes: 'minutes',
+	seconds: 'seconds',
+
+	past: 'ago',	
+		
+	month1: 'januari',
+	month2: 'februari',
+	month3: 'march',
+	month4: 'april',
+	month5: 'may',
+	month6: 'june',
+	month7: 'july',
+	month8: 'august',
+	month9: 'september',
+	month10: 'october',
+	month11: 'november',
+	month12: 'december',
+
+	month1short: 'jan',
+	month2short: 'feb',
+	month3short: 'mrt',
+	month4short: 'april',
+	month5short: 'may',
+	month6short: 'jun',
+	month7short: 'jul',
+	month8short: 'aug',
+	month9short: 'sept',
+	month10short: 'oct',
+	month11short: 'nov',
+	month12short: 'dec',	
+	
+	day1short: 'su',
+	day2short: 'mo',
+	day3short: 'tu',
+	day4short: 'we',
+	day5short: 'th',
+	day6short: 'vr',
+	day7short: 'sa',
+	
+	day1: 'sunday',
+	day2: 'monday',
+	day3: 'tuesday',
+	day4: 'wednesday',
+	day5: 'thursday',
+	day6: 'friday',
+	day7: 'saturday'
+}
