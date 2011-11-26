3c14dcc3d3d4963e34a2d0752ebc1bbe3fb6e4a1
Pieter <pieter@nr8.nl>
2011-11-26 12:29:10 +0100

    Added option useUppercase

--- enum.js
+++ enum.js
@@ -1,9 +1,11 @@
 /**
  * 
  */
-
-util.enum = function(from, to, step, regexp)
+util.enum = function(from, to, step, regexp, opt)
 {
+	if(typeof step === 'undefined') var step = 1
+	if(typeof opt !== 'undefined' && typeof opt.useUppercase !== 'undefined')
+		util.enum.optUseUppercase = opt.useUppercase
 	var ret = [from]
 	var max = from.length 
 	var s = from
@@ -11,18 +13,20 @@ util.enum = function(from, to, step, regexp)
 		do
 		{	
 			/* Take step times */
-			s = util.enumNext(s)
+			s = util.enum.enumNext(s)
 			if(s == to)
 				break
 			if(++c % step == 0 && (typeof regexp == 'undefined' || s.match(regexp)))
 				ret.push(s)
-			c++
 		}
 		while(s != to)
 	ret.push(to)
 	return ret
 }
-util.enumNext = function(str)
+
+util.enum.optUseUppercase = false
+
+util.enum.enumNext = function(str)
 {
 	var max = str.length 
 	var c = 1
@@ -30,9 +34,10 @@ util.enumNext = function(str)
 	/* Count digits from the right that need to be changed */
 	for(c; c <= max; c++)
 	{
-		if(str.charCodeAt(max - c) + 1 % ('z'.charCodeAt(0)-1) > 'z'.charCodeAt(0))
+		if(str.charCodeAt(max - c) + 1 % ('z'.charCodeAt(0)-1) > ('z'.charCodeAt(0)))
 			continue
-		else break 
+		 
+		break 
 	}
 	
 	var ret = ''
@@ -46,29 +51,32 @@ util.enumNext = function(str)
 		if(c > max) ret += '0'
 		else
 		{
-			code = util._enumNext(str, max - c)
+			code = util.enum._enumNext(str, max - c)
 			ret += String.fromCharCode(code)
 		}
 	}
 
 	return ret 
 }
-util._enumNext = function(str, index)
+util.enum._enumNext = function(str, index)
 {
 	var code = str.charCodeAt(index) + 1
-	if(code > '9'.charCodeAt(0) && code < 'a'.charCodeAt(0))
-		code += ("a".charCodeAt(0) - "9".charCodeAt(0) - 1)
+
+	if(util.enum.optUseUppercase)
+	{
+		if(code > '9'.charCodeAt(0) && code < 'A'.charCodeAt(0))
+			code += ("A".charCodeAt(0) - "9".charCodeAt(0) -1)
+		if(code > 'Z'.charCodeAt(0) && code < 'a'.charCodeAt(0))
+			code += ("a".charCodeAt(0) - "Z".charCodeAt(0)-1)
+	}
+	else
+	{
+		if(code > '9'.charCodeAt(0) && code < 'a'.charCodeAt(0))
+			code += ("a".charCodeAt(0) - "9".charCodeAt(0) -1)
+	}
 	if(code > "z".charCodeAt(0))
 		code = "0".charCodeAt(0)
 	return code
 }
 
-util.write = function(str)
-{
-	document.getElementsByTagName('body')[0].innerHTML += '<textarea>' + str + '</textarea>'
-}
-util.alert = function(str)
-{
-	alert(str)
-}
-	util.write(util.enum('1', '10', 1, RegExp(/\w\w/)).join('|'))
+//alert(util.enum('1', '10', 1, void(0), {useUppercase:false}).join('|'))
