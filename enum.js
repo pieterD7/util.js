04e549faea3ddba36d73c95fc380e1c6d1278d73
Pieter <pieter@nr8.nl>
2011-11-26 19:41:18 +0100

    Added example cardgame setup

--- enum.js
+++ enum.js
@@ -3,33 +3,47 @@
  */
 util.enum = function(from, to, opt)
 {
-	if(typeof opt === 'undefined') var opt = {}
+	if(util.isUndef(opt)) var opt = {}
 
-	if(typeof opt.step === 'undefined') var step = 1
+	if(util.isUndef(opt.step)) var step = 1
 	else
-		var step = opt.step
+		var step = opt.step	
+	
+	if(util.isNumber(from) && util.isNumber(to))
+	{
+		var ret = []
+		for(var c = from; c < to; c += step)
+		{
+			ret.push(c)
+		}
+		return ret
+	}	
 		
-	if(typeof opt.regexp === 'object')
+	if(util.isObject(opt.regexp))
 		regexp = opt.regexp
+	else
+		regexp = new RegExp()	
 
-	if(typeof opt.useLowercase !== 'undefined')
+	if(!util.isUndef(opt.useLowercase))
 		util.enum.optUseLowercase = opt.useLowercase	
 		
-	if(typeof opt.useUppercase !== 'undefined')
+	if(!util.isUndef(opt.useUppercase))
 		util.enum.optUseUppercase = opt.useUppercase
 
-	if(typeof opt.useSpecial !== 'undefined')
+	if(!util.isUndef(opt.useSpecial))
 		util.enum.optUseSpecial = opt.useSpecial
 	
 	var cb = false
-	if(typeof opt.onEnumNext === 'function')
+	if(util.isFunction(opt.onEnumNext))
 	{
 		cb = opt.onEnumNext
 		cb(from)
 	}
 	else
+	{
+		cb = null
 		var ret = [from]
-	
+	}
 	if(!util.enum.isValid(from, to))
 		return ret
 	
@@ -43,19 +57,15 @@ util.enum = function(from, to, opt)
 			if(s == to)
 				break
 			if(++c % step == 0 && (typeof regexp == 'undefined' || s.match(regexp)))
-				switch(cb)
-				{
-					case false:
-						ret.push(s)
-						break
-					default:
-						cb(s)
-				}
+				if(typeof cb !== 'function')
+					ret.push(s)
+				else if(typeof cb === 'function')
+					cb(s)
 		}
 		while(s != to)
-	if(!cb)		
+	if(!util.isFunction(cb))		
 		ret.push(to)
-	else
+	else if(util.isFunction(cb))
 		cb(to)
 	return ret
 }
@@ -111,9 +121,102 @@ util.enum.enumNext = function(str)
 
 	return ret 
 }
-util.enum._enumPrev = function()
+util.enum.enumPrev = function(str)
 {
+	var max = str.length 
+	var c = 1
+	
+	/* Count digits from the right that need to be changed */
+	for(c; c <= max; c++)
+	{
+		if(util.enum.optUseSpecial)
+		{
+			if(str.charCodeAt(max - c) + 1 % ('~'.charCodeAt(0)-1) > ('~'.charCodeAt(0)))
+				continue
+		}	
+		else if(util.enum.optUseLowercase)
+		{
+			if(str.charCodeAt(max - c) + 1 % ('z'.charCodeAt(0)-1) > ('z'.charCodeAt(0)))
+				continue
+		}
+		else if(util.enum.optUseUppercase)
+		{
+			if(str.charCodeAt(max - c) + 1 % ('Z'.charCodeAt(0)-1) > ('Z'.charCodeAt(0)))
+				continue
+		}		
+		else if(str.charCodeAt(max - c) + 1 % ('9'.charCodeAt(0)-1) > ('9'.charCodeAt(0))) 
+			continue
+		 
+		break 
+	}
+	
+	var ret = ''
 
+	/* Build answer from the left */	
+	ret = str.substring(0, max - c)		
+
+	/* Add changed digits to the right */
+	for(; c > 0; c--)
+	{
+		if(c > max) ret += '0'
+		else
+		{
+			code = util.enum._enumPrev(str, max - c)
+			ret += String.fromCharCode(code)
+		}
+	}
+
+	return ret 	
+}
+
+util.enum.enumAround = function(from, opt)
+{
+	var ret = []
+	return ret
+}
+
+util.enum._enumPrev = function(str, index)
+{
+	var code = str.charCodeAt(index) - 1
+
+	if(util.enum.optUseSpecial)
+	{
+		if(!util.enum.optUseLowercase)
+		{
+			if(code >= "a".charCodeAt(0) && code <= 'z'.charCodeAt(0))
+				code -= ('z'.charCodeAt(0) - "a".charCodeAt(0))
+		}
+		if(!util.enum.optUseUppercase)
+		{
+			if(code >= 'A'.charCodeAt(0) && code <= 'Z'.charCodeAt(0))
+				code -= ("Z".charCodeAt(0) - "A".charCodeAt(0))
+		}
+		if(code < " ".charCodeAt(0))
+			code = "~".charCodeAt(0)
+	}
+	else
+	{
+		if(util.enum.optUseUppercase)
+		{
+			if(code > '9'.charCodeAt(0) && code < 'A'.charCodeAt(0))
+				code -= ("A".charCodeAt(0) - "9".charCodeAt(0) -1)
+			if(code > 'Z'.charCodeAt(0) && code < 'a'.charCodeAt(0))
+				code -= ("a".charCodeAt(0) - "Z".charCodeAt(0)-1)
+		}
+		else if(util.enum.optUseLowercase)
+		{
+			if(code > '9'.charCodeAt(0) && code < 'a'.charCodeAt(0))
+				code -= ("a".charCodeAt(0) - "9".charCodeAt(0) -1)
+		}
+		else
+		{
+			if(code > '9'.charCodeAt(0) && code < 'A'.charCodeAt(0))
+				code -= ("A".charCodeAt(0) - "9".charCodeAt(0) -1)			
+		}
+		if(code < "0".charCodeAt(0))
+			code = '9'.charCodeAt(0)
+	}
+	return code
 }
 util.enum._enumNext = function(str, index)
 {
@@ -181,18 +284,49 @@ util.enum.isValid = function(from, to)
 	return true	
 }
 
+
+//Little card game setup
 /*
-util.enum('1', '2').forEach(function(pnumber)
+var deck = []
+var set = {A:'spade',B:'coppe', C:'denari', D:'bastoni'}
+
+function card()
 {
-	util.enum(pnumber + '000AZ', pnumber + '000BA', 
+	return this
+}
+card.prototype.display = function()
+{
+	alert(set[this.data.kind] + " " + this.data.name)
+}
+
+util.enum('A', 'D')
+ .forEach(
+	function(kind)
+	{
+		util.enum('AA', 'AD')
+		 .concat(util.enum(2, 10))
+			.forEach(
+				function(n)
+				{
+					var c = new util.struct([card], {kind:kind, name:n, value:n})
+					deck.push(c)
+				}
+			)		
+	}
+)
+*/
+
+/*
+util.enum('1000', '1002').forEach(function(pnumber)
+{
+	util.enum(pnumber + 'AZ', pnumber + 'BB', 
 		{
-		regexp:RegExp(/\d\d\d\d[A-Z]{2}/),
+		regexp:RegExp(/\d{4}[A-Z]{2}/),
 		onEnumNext:function(pcode)
 		{
 			alert(pcode)
 		}
 	})
 })
-
 */
-//alert(util.enum('1000AX', '1000BA').join('|'))
+//alert(util.enum.enumPrev('1000'))
