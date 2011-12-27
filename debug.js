e7c63231eecf27bbc84fae9a14de419f3194fca3
Pieter <pieter@nr8.nl>
2011-12-27 19:46:24 +0100

    Added debug module

--- debug.js
+++ debug.js
@@ -4,15 +4,29 @@
 
 util.debug = {
 		
-	appGetState: function(){return ''},
+	getAppState: function(){return 0},
 	msgContainerSel: '.info'
+}
+
+util.debug.setGetAppState = function(cb)
+{
+	if(util.isFunction(cb))
+		this.getAppState = cb
 }
 
 util.debug.log = function(err)
 {
 	var state = 0
-	if(util.isFunction(util.debug.appGetState))
-		state = util.debug.appGetState()
+	if(util.isFunction(util.debug.getAppState))
+		state = util.debug.getAppState()
+		
 	if(! util.isUndef(util.debug.msgContainerSel))
-		_s(util.debug.msgContainerSel).html(state + ' ' + err)	
+		_s(util.debug.msgContainerSel)
+			.html(
+				'<div class="debugMsgHeader">' + util.defaultStrings.debugHeader  + '</div>' +
+				'<div class="debugMsgText">' + util.defaultStrings.debugMsg + '<br/>' + 
+				document.location.pathname.split('/').slice(2).join('/') + '<br/>' +
+				state + '<br/>' + 
+				'<i>' + err + '</i></div>'
+			)	
 }
