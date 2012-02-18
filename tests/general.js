c797da3b8b9d6dcc39f2652e96df5b93425153a4
Pieter <pieter@nr8.nl>
2012-02-18 05:55:59 +0100

    Fixed regex in test

--- tests/general.js
+++ tests/general.js
@@ -25,16 +25,17 @@ util.test.assertStr(n.name, "pieter's", "Test2")
 
 var n = util.toJson("[{'name':'pieter\'s'},{'name':'lo\\\\pi'},{'name':'Kilo zei:\\\"Hoera!\\\"'}]")
 util.test.assertStr(n[2].name, 'Kilo zei:"Hoera!"', 'Test3')
-util.test.assertStr(n[1].name, "lo\pi", 'Test4')
+util.test.assertStr(n[1].name, "lo\\\\pi", 'Test4')
+util.test.assertStr(n[0].name, "pieter's", 'Test5')
 
 var s = [1, 2, 3, 3].unique().join()
-util.test.assertStr(s, "1,2,3", "Test5")
+util.test.assertStr(s, "1,2,3", "Test6")
 
 var s = [{name:'ik'},{name:'ik'}, {name:'pieter'}].unique('name').joinEach('name', '|')
-util.test.assertStr(s, "ik|pieter", 'Test6')
+util.test.assertStr(s, "ik|pieter", 'Test7')
 
 var s = util.toDirection(5.4, 53, 5.3, 52)
-util.test.assertStr(s, "s", 'Test7')
+util.test.assertStr(s, "s", 'Test8')
 
 var s = util.toXml("<tag>&amp;</tag>").getElementsByTagName('tag')[0].childNodes[0].nodeValue
-util.test.assertStr(s, "&", 'Test8')
+util.test.assertStr(s, "&", 'Test9')
