e5ed4b7b6503a4f91ef43e33326ace778ed20ed4
Pieter <pieter@nr8.nl>
2012-03-23 00:58:02 +0100

    Added sorted alphabetically to country select

--- country.js
+++ country.js
@@ -44,6 +44,7 @@ util.country = {};
 						ii = i - 1
 				})
 				var inps = _sa("select.country")
+				var set = []
 				util.forEach(inps, function(_inp)
 				{
 					var inp = new HTMLElement(_inp)
@@ -54,11 +55,24 @@ util.country = {};
 					inp.appendChild(opt)
 					util.forEach(isos, function(is, i)
 					{
+						set.push({iso:is, txt:iso[ii * 54 + i]})
+					})
+					set.sort(function(a, b)
+					{
+						if(b.txt < a.txt)
+							return 1
+						else if(b.txt > a.txt)
+							return -1
+						return 0
+					})
+					util.forEach(set, function(cntr)
+					{
 						var opt = util.createElement('option')
-						opt.setHtml(iso[ii * 54 + i])
-						opt.val(is)
+						opt.setHtml(cntr.txt)
+						opt.val(cntr.iso)
 						inp.appendChild(opt)
-					})					
+					})
+					
 				})
 			}
 		})
