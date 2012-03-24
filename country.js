7fd708274d83deea3621c845017002d7b62eadd5
Pieter <pieter@nr8.nl>
2012-03-24 20:34:24 +0100

    Improved lines data

--- country.js
+++ country.js
@@ -16,67 +16,79 @@ util.country = {};
 			url:'utiljs/countriesEurope.txt',
 			onSuccess: function(data)
 			{
-				var c = []
-				var iso = []
-				var inPart = false
-				var lines = data.split(RegExp("\n"))
-				util.forEach(lines, function(line)
+				util.eventHandler(function()
 				{
-					line = line.replace("\r", '')
-					if(line.substring(0, 2) == '##')
-						return
-					if(line.substring(0, 2).match(/[#]\w/))
+					var c = []
+					var iso = []
+					var inPart = false
+					var lines = data.split(RegExp("\n"))
+					util.forEach(lines, function(line)
 					{
-						inPart = true
-						c.push(line.replace("#", ""))
-					}
-					else if(line.substring(0, 1) == '#')
-					{
-						inPart = false
-					}
-					else if(inPart)
-					{
-						iso.push(line)
-					}
-				})
-				var isos = iso.splice(0, iso.length / c.length)
-				var ii = false
-				util.forEach(c, function(_locale, i)
-				{
-					if(locale == _locale)
-						ii = i - 1
-				})
-				var inps = _sa("select.country")
-				util.forEach(inps, function(_inp)
-				{
-					var set = []
-					var inp = new HTMLElement(_inp)
-					inp.setHtml('')
-					var opt = util.createElement('option')
-					opt.setHtml(_inp.getAttribute('placeholder'))
-					opt.addClassName('valueishint') // doesn't do much (Chrome)
-					opt.val('')
-					inp.appendChild(opt)
-					util.forEach(isos, function(is, i)
-					{
-						set.push({iso:is, txt:iso[ii * isos.length + i]})
+						line = line.replace("\r", '')
+						if(line.substring(0, 2) == '##')
+							return
+						if(line.substring(0, 2).match(/[#]\w/))
+						{
+							inPart = true
+							c.push(line.replace("#", ""))
+						}
+						else if(line.substring(0, 1) == '#')
+						{
+							inPart = false
+						}
+						else if(inPart)
+						{
+							iso.push(line)
+						}					
 					})
-					set.sort(function(a, b)
-					{
-						if(b.txt < a.txt)
-							return 1
-						else if(b.txt > a.txt)
-							return -1
-						return 0
-					})
-					util.forEach(set, function(cntr)
-					{
-						var opt = util.createElement('option')
-						opt.setHtml(cntr.txt)
-						opt.val(cntr.iso)
-						inp.appendChild(opt)
-					})
+					// Check ok?
+					if(!(c.length && iso.length && iso.length > c.length) ||
+						(iso.length % c.length != 0))
+							throw(new util.error(
+								"Error reading " + iso.length + 
+								" lines in  " + 
+								c.length + " equal sections"))
 					
+					var isos = iso.splice(0, iso.length / c.length)
+					var ii = false
+					util.forEach(c, function(_locale, i)
+					{
+						if(locale == _locale)
+							ii = i - 1
+					})				
+					
+					var inps = _sa("select.country")
+					util.forEach(inps, function(_inp)
+					{
+						var set = []
+						var inp = new HTMLElement(_inp)
+						inp.setHtml('')
+						var opt = util.createElement('option')
+						opt.setHtml(_inp.getAttribute('placeholder'))
+						opt.addClassName('valueishint') // doesn't do much (Chrome)
+						opt.val('')
+						inp.appendChild(opt)
+						util.forEach(isos, function(is, i)
+						{
+							set.push({iso:is, txt:iso[ii * isos.length + i]})
+						})
+						set.sort(function(a, b)
+						{
+							if(b.txt < a.txt)
+								return 1
+							else if(b.txt > a.txt)
+								return -1
+							return 0
+						})
+						util.forEach(set, function(cntr)
+						{
+							var opt = util.createElement('option')
+							opt.setHtml(cntr.txt)
+							opt.val(cntr.iso)
+							inp.appendChild(opt)
+						})
+						
+					})
 				})
 			}
 		})
@@ -85,5 +97,5 @@ util.country = {};
 
 util.ready(function()
 {
-	util.country.init(util._selectedLocale)
+	util.country.init(util.selectedLocale)
 })
