74a90858983b79e3003529c16d1ac69d2acb8aa4
Pieter <pieter@nr8.nl>
2012-05-28 00:32:53 +0200

    Improved version

--- canvas.js
+++ canvas.js
@@ -11,25 +11,31 @@ util.canvas = {
 {
 	"use strict"
 	
-	util.canvas.Canvas = function(o)
+	util.canvas.Canvas = function(o, width, height)
 	{
 		this.o = o
 		this.context = null
 		this.data = null
-		this.initData()
+		this.width = width 
+		this.height = height 
+		this.initData(this.width, this.height)
 		return this
 	}
 	
-	util.canvas.Canvas.prototype.initData = function(left, top)
+	util.canvas.Canvas.prototype.initData = function(width, height)
 	{
 		this.context = this.o.getContext('2d')
-		this.data = this.context.createImageData(1,1)
+		this.data = this.context.createImageData(width, height)
 	}
 	
 	util.canvas.Canvas.prototype.setPixel = function(pix, x, y)
 	{
-		this.data = this.context.getImageData(x,y,1,1)
-		this.context.putImageData(pix.getPixelData(this.data), x, y)
+		this.data = pix.putPixelData(this.data, x, y)
+	}
+
+	util.canvas.Canvas.prototype.draw = function()
+	{
+		this.context.putImageData(this.data, 0, 0)		
 	}
 	
 	util.canvas.Pixel = function(r,g,b,a)
@@ -41,26 +47,25 @@ util.canvas = {
 		return this
 	}
 	
-	util.canvas.Pixel.prototype.getPixelData = function(data)
+	util.canvas.Pixel.prototype.putPixelData = function(data, x, y)
 	{
-		data.data[0] = this.r | 0 
-		data.data[1] = this.g | 0 
-		data.data[2] = this.b | 0 
-		data.data[3] = this.a | 0
+		var index = (x + y * data.width) * 4;
+		data.data[index+0] = this.r | 0 
+		data.data[index+1] = this.g | 0 
+		data.data[index+2] = this.b | 0 
+		data.data[index+3] = this.a | 0
 		return data
 	}
 	
-	util.canvas.init = function(sel)
+	util.canvas.init = function(sel, width, height)
 	{
 		var cs = _sa(sel)
 		util.forEach(cs, function(cvas)
 		{
-			var c = new util.canvas.Canvas(cvas)
+			var c = new util.canvas.Canvas(cvas, width, height)
 			util.canvas.cvasses.push(c)
 		})
+		return util.canvas.cvasses
 	}
 	
 })()
-
-
-})
