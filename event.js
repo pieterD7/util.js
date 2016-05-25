dff5904a9f43e84ac74d7a3bfef921905c519dc6
Pieter <pieter@nr8.nl>
2016-05-25 11:45:10 +0200

    Improved indentation

--- event.js
+++ event.js
@@ -1,4 +1,4 @@
-(function () {
+	(function () {
     "use strict";
 
 	/**
@@ -22,19 +22,19 @@
 	
 	util.fireEvent = function(element, event)
 	{
-	    if (document.createEventObject)
-	    {
-		    // dispatch for IE
-		    var evt = document.createEventObject();
-		    return element.fireEvent('on'+event,evt)
-	    }
-	    else
-	    {
-		    // dispatch for firefox + others
-		    var evt = document.createEvent("HTMLEvents");
-		    evt.initEvent(event, true, true ); // event type,bubbling,cancelable
-		    return !element.dispatchEvent(evt);
-	    }
+		if (document.createEventObject)
+		{
+			// dispatch for IE
+			var evt = document.createEventObject();
+			return element.fireEvent('on'+event,evt)
+		}
+		else
+		{
+		// dispatch for firefox + others
+		var evt = document.createEvent("HTMLEvents");
+		evt.initEvent(event, true, true ); // event type,bubbling,cancelable
+		return !element.dispatchEvent(evt);
+		}
 	}
 	
 	util.eventObjectToPos = function(ee)
